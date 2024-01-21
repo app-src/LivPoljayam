fcaf96b47bc5e7581c702305624fbbf9af993853
ashish <ashishthehulk@gmail.com>
2024-01-21 18:44:38 +0530

    -fuckFirebase

--- app/src/main/java/com/example/livpol/DataActivity.kt
+++ app/src/main/java/com/example/livpol/DataActivity.kt
@@ -1,27 +1,17 @@
 package com.example.livpol
 
 import android.app.Notification
-import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.graphics.BitmapFactory
-import android.graphics.Color
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.OnBackPressedDispatcher
 import com.example.livpol.databinding.ActivityDataBinding
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
-import com.google.firebase.database.getValue
 
 @Suppress("DEPRECATION")
 class DataActivity : AppCompatActivity() {
@@ -32,7 +22,7 @@ class DataActivity : AppCompatActivity() {
 //    private lateinit var people: TextView
 //    private lateinit var minutes: TextView
     private lateinit var notificationManager: NotificationManager
-    private lateinit var notificationChannel: NotificationChannel
+//    private lateinit var notificationChannel: NotificationChannel
 //    private lateinit var circles: ImageView
     private lateinit var context: Context
     private var notified = false
@@ -52,78 +42,11 @@ class DataActivity : AppCompatActivity() {
 
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // Write a message to the database
         val database = Firebase.database
-        val myRef = database.getReference("people")
-
-        myRef.addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-
-                val value = snapshot.getValue<HashMap<String,String>>().toString()
-                Log.d("hi", "Value is: " + value)
-                binding.people.text = value.replaceFirst("{Name=","").replaceFirst("}","") + " \npeople"
-               binding.minutes.text = (value.replaceFirst("{Name=","").replaceFirst("}","").toInt()*2).toString() + " \nminutes"
-                var pendingIntent: PendingIntent? = null
-                pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    PendingIntent.getActivity(
-                        context,
-                        0,
-                        intent,
-                        PendingIntent.FLAG_MUTABLE
-                    )
-                } else {
-                    PendingIntent.getActivity(
-                        context,
-                        0,
-                        intent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                    )
-                }
-
-//                val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-                var v = value.replaceFirst("{Name=","").replaceFirst("}","").toInt()
-                runOnUiThread(Runnable{
-                   binding.peopleProgressBar.progress = v
-                    binding.minutesProgressBar.progress = v*2
-                })
-
-                // RemoteViews are used to use the content of
-                // some different layout apart from the current activity layout
-//                val contentView = RemoteViews(packageName, R.layout.activity_after_notification)
-
-                if(!notified){
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
-                        notificationChannel.enableLights(true)
-                        notificationChannel.lightColor = Color.GREEN
-                        notificationChannel.enableVibration(false)
-                        notificationManager.createNotificationChannel(notificationChannel)
-
-                        builder = Notification.Builder(context, channelId)
-                            .setSmallIcon(R.drawable.ic_launcher_background)
-                            .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_background))
-                            .setContentIntent(pendingIntent)
-                    } else {
-
-                        builder = Notification.Builder(context)
-                            .setSmallIcon(R.drawable.ic_launcher_background)
-                            .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_background))
-                            .setContentIntent(pendingIntent)
-                    }
-                    notificationManager.notify(1234, builder.build())
-                    notified = true
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w("ji", "Failed to read value.", error.toException())
-            }
-
-        })
-
+        val myRef = database.getReference("message")
+        myRef.setValue("Hello, World!")
     }
+
     private fun createBarChart() {
 
         val entries = ArrayList<BarEntry>()
--- app/src/main/java/com/example/livpol/SearchByEpicNumberActivity.kt
+++ app/src/main/java/com/example/livpol/SearchByEpicNumberActivity.kt
@@ -2,14 +2,10 @@ package com.example.livpol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.livpol.databinding.ActivitySearchByEpicNumberBinding
 
 class SearchByEpicNumberActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivitySearchByEpicNumberBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySearchByEpicNumberBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_search_by_epic_number)
     }
 }
